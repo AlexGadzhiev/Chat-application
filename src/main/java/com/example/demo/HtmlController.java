ba8bb862ed37bf8555fff5db6dfb246732d9a01c
gadzhiev <gadzhiev@iqmen.ru>
2022-04-05 16:22:04 +0300

    Replaced adding username with passwords and histories into tables in database postgres. Hope not to find buges

--- src/main/java/com/example/demo/HtmlController.java
+++ src/main/java/com/example/demo/HtmlController.java
@@ -16,9 +16,8 @@ import java.util.List;
 public class HtmlController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    private HashMap<String, List<String>> histories = new HashMap<>();
-    private HashMap<String, String> accounts = new HashMap<>();
     private String lastUser;
+    private String current_history;
 
     private String makeString(int a, int b, int total, String op) {
         return a + " " + op + " " + b + " = " + total;
@@ -28,23 +27,34 @@ public class HtmlController {
     @ResponseBody
     public ResponseEntity<?> getInfo(@RequestParam("a") Integer a, @RequestParam("b") Integer b,
                                      @RequestParam("action") String action) throws IOException {
+        List<UserHistory> histories = jdbcTemplate.query("SELECT * from histories", BeanPropertyRowMapper.newInstance(UserHistory.class));
+        for(UserHistory user : histories){
+            if(user.getUsername().equals(lastUser)){
+                current_history = user.getHistory();
+                break;
+            }
+        }
         com.example.demo.Operation obj = new com.example.demo.Operation(a, b);
         switch (action) {
             case "plus":
                 if (lastUser != null)
-                    histories.get(lastUser).add(makeString(a, b, a + b, "+"));
+                    jdbcTemplate.update("UPDATE histories SET history=? WHERE username=?",
+                            current_history + makeString(a, b, a + b, "+") + ", ", lastUser);
                 return ResponseEntity.ok().body(obj.plus());
             case "minus":
                 if (lastUser != null)
-                    histories.get(lastUser).add(makeString(a, b, a - b, "-"));
+                    jdbcTemplate.update("UPDATE histories SET history=? WHERE username=?",
+                            current_history + makeString(a, b, a - b, "-") + ", ", lastUser);
                 return ResponseEntity.ok().body(obj.minus());
             case "div":
                 if (lastUser != null)
-                    histories.get(lastUser).add(makeString(a, b, a / b, "/"));
+                    jdbcTemplate.update("UPDATE histories SET history=? WHERE username=?",
+                            current_history + makeString(a, b, a / b, "/") + ", ", lastUser);
                 return ResponseEntity.ok().body(obj.div());
             case "times":
                 if (lastUser != null)
-                    histories.get(lastUser).add(makeString(a, b, a * b, "*"));
+                    jdbcTemplate.update("UPDATE histories SET history=? WHERE username=?",
+                            current_history + makeString(a, b, a * b, "*") + ", ", lastUser);
                 return ResponseEntity.ok().body(obj.times());
         }
         return ResponseEntity.ok().body(null);
@@ -53,38 +63,48 @@ public class HtmlController {
     @GetMapping(value = "/history")
     @ResponseBody
     public ResponseEntity<?> getHistory() {
-        if (lastUser != null) {
-            return ResponseEntity.ok().body("Username's name is: " + lastUser + '\n' +
-                    "Username's history of operations: " + histories.get(lastUser));
-        } else {
+        if(lastUser == null){
             return ResponseEntity.ok().body("Sorry, don't have one");
         }
+        List<UserHistory> histories = jdbcTemplate.query("SELECT * from histories", BeanPropertyRowMapper.newInstance(UserHistory.class));
+        for(UserHistory user : histories){
+            if(user.getUsername().equals(lastUser)){
+                current_history = user.getHistory();
+                break;
+            }
+        }
+        if(current_history.isEmpty())
+            return ResponseEntity.ok().body(null);
+        return ResponseEntity.ok().body(current_history.substring(0, current_history.length() - 2));
     }
 
     @PostMapping(value = "/login")
     @ResponseBody
     public ResponseEntity<?> login(@RequestBody User user) {
-        if(!accounts.containsKey(user.getUsername())){
-            return ResponseEntity.ok().body("You have to sign up!");
-        } else if(lastUser == null && accounts.get(user.getUsername()) != user.getPassword()){
-            return ResponseEntity.ok().body("Wrong password");
-        } else if(lastUser == null){
-            lastUser = user.getUsername();
-            histories.put(user.getUsername(), new ArrayList<>());
-            return ResponseEntity.ok().body("You have successfully logged in");
-        } else if(lastUser.equals(user.getUsername())){
-            return ResponseEntity.ok().body("You're already logged in!");
+        List<User> userList = jdbcTemplate.query("SELECT * from accounts", BeanPropertyRowMapper.newInstance(User.class));
+        if (lastUser != null) {
+            if (lastUser.equals(user.getUsername())) {
+                return ResponseEntity.ok().body("You're already logged in!");
+            } else if (!userList.contains(user)) {
+                return ResponseEntity.ok().body("Wrong username or password");
+            }
+            return ResponseEntity.ok().body("Try again");
         }
-        return ResponseEntity.ok().body("Try again");
+        for(User curUser : userList){
+            if(curUser.getUsername().equals(user.getUsername()) && user.getPassword().equals(curUser.getPassword())){
+                lastUser = user.getUsername();
+                return ResponseEntity.ok().body("You have successfully logged in");
+            }
+        }
+        return ResponseEntity.ok().body("Wrong login or password");
     }
 
     @GetMapping(value = "/logout")
     @ResponseBody
     public ResponseEntity<?> logout(@RequestParam("name") String name) {
-        if(!name.equals(lastUser))
+        if (name == null || !name.equals(lastUser))
             return ResponseEntity.ok().body("Try again");
         else {
-            histories.get(lastUser).clear();
             lastUser = null;
             return ResponseEntity.ok().body("You have successfully logged out");
         }
@@ -92,18 +112,37 @@ public class HtmlController {
 
     @PostMapping(value = "/signup")
     @ResponseBody
-    public ResponseEntity<?> signup(@RequestBody User user){
-        if(accounts.containsKey(user.getUsername())){
+    public ResponseEntity<?> signup(@RequestBody User user) {
+        List<User> userList = jdbcTemplate.query("SELECT * from accounts", BeanPropertyRowMapper.newInstance(User.class));
+        if (userList.contains(user)) {
             return ResponseEntity.ok().body("You're already signed up!");
         }
-        accounts.put(user.getUsername(), user.getPassword());
-        return ResponseEntity.ok().body("You have successfully signed up");
+        jdbcTemplate.update("INSERT INTO histories (username, history) VALUES(?,?)",
+                user.getUsername(), " ");
+        jdbcTemplate.update("INSERT INTO accounts (username, password) VALUES(?,?)",
+                user.getUsername(), user.getPassword());
+        return ResponseEntity.ok().body("You have successfully signed up!");
     }
 
-    @GetMapping(value = "/run")
-    @ResponseBody
-    public ResponseEntity<?> run(){
-        List<User> userList = jdbcTemplate.query("SELECT * from accounts", BeanPropertyRowMapper.newInstance(User.class));
-        return ResponseEntity.ok().body(null);
-    }
+//    @GetMapping(value = "/run")
+//    @ResponseBody
+//    public ResponseEntity<?> run(){
+//        List<User> userList = jdbcTemplate.query("SELECT * from accounts", BeanPropertyRowMapper.newInstance(User.class));
+//        return ResponseEntity.ok().body(null);
+//    }
+
+//    CREATE TABLE accounts (
+//            user_id serial PRIMARY KEY,
+//            username VARCHAR ( 50 ) UNIQUE NOT NULL,
+//            password VARCHAR ( 50 ) NOT NULL,
+//            email VARCHAR ( 255 ) UNIQUE NOT NULL,
+//            created_on TIMESTAMP NOT NULL,
+//            last_login TIMESTAMP
+//    );
+
+//    CREATE TABLE histories (
+//            user_id INT NULL,
+//            username VARCHAR ( 50 ) UNIQUE NOT NULL,
+//            history VARCHAR ( 50 ) NOT NULL
+//    );
 }
---
+++ src/main/java/com/example/demo/UserHistory.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class UserHistory {
+    private String username;
+    private String history;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+}
